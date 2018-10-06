3315996c545aa1c9f0a3ac91acdde7d53d17c3bd
JJ Ng <ngjunjie1991@gmail.com>
2018-10-06 12:16:05 -0700

    Added exception handling

--- chatbot/whisman_butler.py
+++ chatbot/whisman_butler.py
@@ -34,18 +34,24 @@ class Butler(Client):
                           config.TEMPLATEDIR, config.OUTPUTDIR, config.HORIZ_ALIGN_THRESH)
     curr_door_state, _ = proc.detectGarageDoorState(img)
     msg_txt = "Garage door is " + curr_door_state.name + ' at ' + time.asctime()
-    self.sendLocalImage(path_to_img,
-                        message=Message(text=msg_txt), thread_id=thread_id, thread_type=thread_type)
+    try:
+      self.sendLocalImage(path_to_img,
+                          message=Message(text=msg_txt), thread_id=thread_id, thread_type=thread_type)
+    except (FBchatException, e):
+      print("FBchatException caught!", e.fb_error_message)
 
   def closeGarageReminder(self, elapsed):
     msg_txt = "Garage has been opened for " + \
         str(timedelta(seconds=int(elapsed))) + \
         '. Chippie feels cold please close the FUCKING GARAGE.'
     img_to_send = self.createScaryImage()
-    msg_id = self.sendLocalImage(img_to_send, message=Message(
-        text=msg_txt), thread_id=config.JJ_ID, thread_type=ThreadType.USER)
-    print("sent message id:", msg_id)
-    self.reactToMessage(msg_id, MessageReaction.ANGRY)
+    try:
+      msg_id = self.sendLocalImage(img_to_send, message=Message(
+          text=msg_txt), thread_id=config.JJ_ID, thread_type=ThreadType.USER)
+      print("sent message id:", msg_id)
+      self.reactToMessage(msg_id, MessageReaction.ANGRY)
+    except (FBchatException, e):
+      print("FBchatException caught!", e.fb_error_message)
 
   def createScaryImage(self):
     img1 = cv2.imread(
